9cbf2c53107ff916e710172749a50d3b558ec01a
mvac-x <bymaahh@gmail.com>
2017-06-23 03:13:06 -0300

    upando alguns scripts

---
+++ js/mostrarredes.js
@@ -0,0 +1,59 @@
+var sociais = document.querySelectorAll(".formgroupface");
+
+
+	sociais.forEach(function(sociaisFace){
+		socialFace.addEventListener("click", function(){
+
+			document.getElementById("sociaisFace").style.display = "block";
+    		 
+		});
+
+
+	});
+
+
+
+
+
+	sociais.forEach(function(sociaisWpp){
+		socialWpp.addEventListener("click", function(){
+
+			document.getElementById("sociaisWpp").style.display = "block";
+    		 
+		});
+
+
+	});
+
+
+
+
+
+
+		sociais.forEach(function(sociaisWpp){
+		socialViber.addEventListener("click", function(){
+
+			document.getElementById("sociaisViber").style.display = "block";
+    		 
+		});
+
+
+	});
+
+
+
+	sociais.forEach(function(sociaisTele){
+		socialTelegram.addEventListener("click", function(){
+
+			document.getElementById("sociaisTele").style.display = "block";
+    		 
+		});
+
+
+	});
+
+
+
+
+
+
---
+++ js/removersocial.js
@@ -0,0 +1,59 @@
+var removerSociais = document.querySelectorAll("#removerSocialFace");
+
+
+removerSociais.forEach(function(removerSocial){
+	removerSocial.addEventListener("click", function(){
+		
+		document.getElementById("sociaisFace").style.display = "none";
+
+	});
+
+});
+
+
+
+var removerSociais = document.querySelectorAll("#removerSocialWpp");
+
+
+removerSociais.forEach(function(removerSocial){
+	removerSocial.addEventListener("click", function(){
+		
+		document.getElementById('sociaisWpp').style.display = "none";
+
+	});
+
+});
+
+
+var removerSociais = document.querySelectorAll("#removerSocialViber");
+
+
+removerSociais.forEach(function(removerSocial){
+	removerSocial.addEventListener("click", function(){
+		
+		document.getElementById('sociaisViber').style.display = "none";
+	});
+
+});
+
+
+
+var removerSociais = document.querySelectorAll("#removerSocialTele");
+
+
+removerSociais.forEach(function(removerSocial){
+	removerSocial.addEventListener("click", function(){
+		
+		document.getElementById('sociaisTele').style.display = "none";
+
+	});
+
+});
+
+
+
+
+
+
+
+
